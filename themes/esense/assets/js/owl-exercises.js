d6b2ff7443a736035778918d522583f90213e3a5
FlorinZarafu <zrflorin@gmail.com>
2020-10-15 12:54:47 +0300

    Deleted jquery function in owl carousel js files

--- themes/esense/assets/js/owl-exercises.js
+++ themes/esense/assets/js/owl-exercises.js
@@ -1,22 +1,20 @@
-jQuery(function() {
-    $('.exercises-owl-carousel').owlCarousel({
-        stagePadding: 80,
-        loop:true,
-        margin:10,
-        nav:false,
-        autoplay:true,
-        autoplayTimeout:2000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:2
-            }
+$('.exercises-owl-carousel').owlCarousel({
+    stagePadding: 80,
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
         }
-    })
+    }
 })
