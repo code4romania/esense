292383730dc3afd72498afa2527c0862a004cc2a
FlorinZarafu <zrflorin@gmail.com>
2020-10-15 12:55:39 +0300

    Added owl carousel small records plugin for exercises and exercises details

---
+++ themes/esense/assets/js/owl-exercise-details.js
@@ -0,0 +1,20 @@
+$('.exercise-detailes-owl-carousel').owlCarousel({
+    stagePadding: 0,
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
