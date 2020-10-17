8b6fbbf4e3d4acfca1c1bfabdc250155cf338f5c
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-10-18 01:19:21 +0300

    Added profile plugin and connected it to the user plugin event to create a user profile when a user registers.

---
+++ plugins/genuineq/profile/updates/extend_user_plugin_users_table.php
@@ -0,0 +1,30 @@
+<?php namespace Genuineq\Profile\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class ExtendUserPluginUsersTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->integer('profile_id')->unsigned()->nullable();
+            $table->string('profile_type')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        if (
+            Schema::hasTable('users')
+            && Schema::hasColumns('users', ['profile_id', 'profile_type'])
+        ) {
+            Schema::table('users', function ($table) {
+                $table->dropColumn(['profile_id', 'profile_type']);
+            });
+        }
+    }
+
+}
