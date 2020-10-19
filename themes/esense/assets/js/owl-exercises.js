d6d128628bc44169e58435b25637180da5f3a087
FlorinZarafu <zrflorin@gmail.com>
2020-10-19 10:32:44 +0300

    Added timeline and slider arrows in student-profile

--- themes/esense/assets/js/owl-exercises.js
+++ themes/esense/assets/js/owl-exercises.js
@@ -3,10 +3,11 @@ $(document).ready(function () {
         stagePadding: 80,
         loop:true,
         margin:10,
-        nav:false,
+        nav:true,
         autoplay:true,
         autoplayTimeout:2000,
         autoplayHoverPause:true,
+        navText : ["<div><i class='fas fa-chevron-left text-secondary'></i></div>","<div><i class='fas fa-chevron-right text-secondary'></i></div>"],
         responsive:{
             0:{
                 items:1
