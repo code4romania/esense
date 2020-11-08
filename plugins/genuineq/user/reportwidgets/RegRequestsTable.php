6c3d0b31cff3c0e0d9566bf529dbc81e79545e0c
LucianDex <remindfwd@gmail.com>
2020-11-08 11:42:05 +0200

    Updated RegRequestsTable widget
    - fixed list select all
    - fixed activation function
    - added orderBy created_at

--- plugins/genuineq/user/reportwidgets/RegRequestsTable.php
+++ plugins/genuineq/user/reportwidgets/RegRequestsTable.php
@@ -23,7 +23,7 @@ class RegRequestsTable extends ReportWidgetBase
             $this->vars['labelRegRequestsTable'] = Lang::get('genuineq.user::lang.reportwidgets.reg_requests_table.frontend.label_registration_requests');
 
             /** Get no of inactive user accounts (== user requests) from database  */
-            $this->vars['userRequests'] = UserModel::where('is_activated', 0)->get();
+            $this->vars['userRequests'] = UserModel::where('is_activated', 0)->orderBy('created_at', 'DESC')->get();
 
         } catch (Exception $ex) {
             $this->vars['error'] = $ex->getMessage();
@@ -52,11 +52,12 @@ class RegRequestsTable extends ReportWidgetBase
      */
     public function onRequestForm()
     {
-        /* Get all user model fields from DB */
+        /* Get all user model fields from DB by selected id */
         $userModel = UserModel::find(post('record_id'));
 
         /* Add userModel variable to modal view */
         $this->vars['userModel'] = $userModel;
+
 
         return $this->makePartial('preview_request');
 
@@ -67,23 +68,44 @@ class RegRequestsTable extends ReportWidgetBase
      ***********************************************/
 
     /**
-     * Function that keeps the logic for Activate account
+     * Function that Activate account on request
      * @return mixed
      */
     public function onRequest()
     {
-        if ($userModel = UserModel::find(post('record_id'))) {
-            $userModel->is_activated = 1;
-            $userModel->save();
+        if ([] != post('record_id')) {
+            $this->activateUser(post('record_id'));
+            /** Go to show success message */
+            goto success;
 
+        }elseif ([] != post('checked')) {
+
+            /** Iterate IDs and get the corresponding user, then activate it */
+            foreach (post('checked') as $accountId) {
+                $this->activateUser($accountId);
+            }
+
+            success:
             Flash::success(Lang::get('genuineq.user::lang.reportwidgets.reg_requests_table.flash.success'));
 
+            /* Refreshing the page */
             return Redirect::refresh();
 
         } else {
-            Flash::error( Lang::get('genuineq.user::lang.reportwidgets.reg_requests_table.flash.fail') );
+            Flash::error(Lang::get('genuineq.user::lang.reportwidgets.reg_requests_table.flash.fail'));
+            return;
         }
     }
 
+    /**
+     * Function that Activate account by user ID
+     * @param $accountId
+     */
+    private function activateUser($accountId)
+    {
+        $userModel = UserModel::find($accountId);
+        $userModel->is_activated = 1;
+        $userModel->save();
+    }
 
 }
