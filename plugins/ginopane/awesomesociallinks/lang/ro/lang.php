d31d85a9b60de9f0c1e22eae2ada4d3510d07f16
LucianDex <remindfwd@gmail.com>
2020-11-17 20:27:15 +0200

    Updated ginopane/AwesomeSocialLinks
    - added RO translation

--- plugins/ginopane/awesomesociallinks/lang/ro/lang.php
+++ plugins/ginopane/awesomesociallinks/lang/ro/lang.php
@@ -2,37 +2,37 @@
 
 return [
     'plugin' => [
-        'name' => 'Awesome Social Links',
-        'description' => 'A list of social icons (or any other branded icons) associated with your website'
+        'name' => 'Legături situri socializare',
+        'description' => 'O listă de iconițe ale siturilor de socializare (sau orice alte iconițe de marcă) asociate site-ului dvs. web'
     ],
     'settings' => [
-        'name' => 'Awesome Social Links',
-        'description' => 'Add, remove item, rearrange the list of icons',
+        'name' => 'Legături situri socializare',
+        'description' => 'Adaugă, elimină elementul, rearanjează lista de iconițe',
 
-        'list_item' => 'List Item',
-        'list_item_prompt' => 'Add a New List Item',
+        'list_item' => 'Articol din Listă',
+        'list_item_prompt' => 'Adaugă un Articol Nou în Listă ',
 
-        'list_item_icon' => 'Icon',
-        'list_item_icon_placeholder' => 'Please select an icon',
+        'list_item_icon' => 'Iconiță',
+        'list_item_icon_placeholder' => 'Te rugăm să selectezi o iconiță',
 
-        'list_item_name' => 'Name',
-        'list_item_name_placeholder' => 'List item name',
+        'list_item_name' => 'Nume',
+        'list_item_name_placeholder' => 'Numele articolului din listă',
 
-        'list_item_url' => 'Link',
-        'list_item_url_placeholder' => 'List item URL'
+        'list_item_url' => 'Legătură',
+        'list_item_url_placeholder' => 'Legătura articolului din listă'
     ],
     'components' => [
         'link_list' => [
-            'name' => 'Icon List',
-            'description' => 'A list of available icons',
-            'link_settings_group' => 'Link Settings',
-            'link_target_title' => 'Link Target',
-            'link_target_description' => 'The default target attribute for your links'
+            'name' => 'Lista de iconițe',
+            'description' => 'Lista de iconițe disponibile',
+            'link_settings_group' => 'Setări legătură',
+            'link_target_title' => 'Ținta legăturii',
+            'link_target_description' => 'Atributul țintă implicit pentru legăturile tale'
         ]
     ],
     'validation' => [
-        'name_required' => 'Please make sure to name every list item',
-        'url_required' => 'Please make sure to set a URL for every list item',
-        'url_must_be_valid' => 'Link value must be a valid URL'
+        'name_required' => 'Te rugăm să te asiguri că denumești fiecare articol din listă',
+        'url_required' => 'Te rugăm să te asiguri că setezi o adresă URL pentru fiecare articol din listă',
+        'url_must_be_valid' => 'Valoarea legăturii trebuie sa fie un URL valid'
     ]
 ];
