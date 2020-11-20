dc90d2785858027d5046353f1ff9022e73ac93ad
LucianDex <remindfwd@gmail.com>
2020-11-20 21:29:34 +0200

    Updated zen/ Smart Storage Cleaner
    - added RO translation

---
+++ plugins/zen/cleaner/lang/ro/lang.php
@@ -0,0 +1,10 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Curățător inteligent spațiu stocare',
+        'description' => 'Elimină imaginile neutilizate din spațiul de stocare',
+    ],
+    'clean_btn' => 'Curăță spațiul de stocare',
+    'deleted_count' => 'Număr de elemente șterse',
+    'deleted_size' => 'Dimensiunea fișierelor șterse',
+    'clean_success' => 'Spațiul de stocare este curat!',
+];
