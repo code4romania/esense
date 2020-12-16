0461c3ef397fa1247f5d57b592e72c2892118637
LucianDex <remindfwd@gmail.com>
2020-12-16 22:07:40 +0200

    [in progress] created partial for displaying users in profile

--- plugins/genuineq/profile/controllers/Schools.php
+++ plugins/genuineq/profile/controllers/Schools.php
@@ -1,17 +1,19 @@
 <?php namespace Genuineq\Profile\Controllers;
 
-use Backend\Classes\Controller;
 use BackendMenu;
+use Backend\Classes\Controller;
 
 class Schools extends Controller
 {
     public $implement = [
         'Backend\Behaviors\ListController',
-        'Backend\Behaviors\FormController'
+        'Backend\Behaviors\FormController',
+        'Backend\Behaviors\RelationController',
     ];
 
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
+    public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
     {
