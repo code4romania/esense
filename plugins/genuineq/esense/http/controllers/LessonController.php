1ec239829af91979e274a821c707b3d7032df423
Lupu Gheorghe <gheorghelupu17@gmail.com>
2022-10-29 04:44:07 +0300

    fix auth for integration

--- plugins/genuineq/esense/http/controllers/LessonController.php
+++ plugins/genuineq/esense/http/controllers/LessonController.php
@@ -3,41 +3,30 @@
 use Log;
 use Lang;
 use Request;
+use Auth;
 use Exception;
 use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
-use Genuineq\JWTAuth\Classes\JWTAuth;
-use Genuineq\Students\Models\Student;
 use Genuineq\Timetable\Models\Lesson;
 
 class LessonController extends Controller
 {
     /**
      * Starts a trip for the authenticated user
-     *
-     * @param JWTAuth $auth
      * @param Request $request
      *
      * @return Illuminate\Http\Response
      */
-    public function __invoke(JWTAuth $auth, Request $request)
+    public function __invoke(Request $request)
     {
         /** Extract the student. */
-        $student = $auth->user()->profile->students->where('id', post('student'))->first();
+        $student =  Auth::user()->profile->students->where('id', post('student'))->first();
 
         /** Validate the access to the student. */
         if (!$student) {
             return response()->json(
                 ['error' => Lang::get('genuineq.esense::lang.components.lessonsActions.message.no_access'),],
-                Response::HTTP_FORBIDDEN
-            );
-        }
-
-        /** Validate the date. */
-        if (Carbon::now()->format('Y-m-d') != post('date')) {
-            return response()->json(
-                ['error' => Lang::get('genuineq.esense::lang.components.lessonsActions.message.invalid_date'),],
                 Response::HTTP_FORBIDDEN
             );
         }
@@ -52,7 +41,7 @@ class LessonController extends Controller
 
         /** Validate the category. */
         /** Extract the exercises categories. */
-        $categoriesSlugs = $auth->user()->profile->exercises_categories->pluck('slug')->toArray();
+        $categoriesSlugs =Auth::user()->profile->exercises_categories->pluck('slug')->toArray();
         if (!in_array(post('category'), $categoriesSlugs)) {
             return response()->json(
                 ['error' => Lang::get('genuineq.esense::lang.components.lessonsActions.message.invalid_category') . implode(", ", $categoriesSlugs),],
@@ -62,14 +51,14 @@ class LessonController extends Controller
 
         /** Create a new Lesson. */
         $lesson = Lesson::create([
-            'day' => post('date'),
+            'day' => Carbon::now()->format('Y-m-d'),
             'start_hour' => post('startHour'),
             'end_hour' => post('endHour'),
             'duration' => post('duration') ?? (strtotime(post('endHour')) - strtotime(post('startHour'))),
-            'title' => ((post('title')) ? (post('title')) : ('')),
+            'title' => post('category').'_'.Carbon::now()->format('Y-m-d'),
             'description' => ((post('description')) ? (post('description')) : ('')),
             'feedback' => ((post('feedback')) ? (post('feedback')) : ('')),
-            'connection_id' => $student->connections->where('specialist_id', $auth->user()->profile->id)->first()->id,
+            'connection_id' => $student->connections->where('specialist_id', Auth::user()->profile->id)->first()->id,
             'category' => post('category')
         ]);
 
--- plugins/genuineq/esense/routes.php
+++ plugins/genuineq/esense/routes.php
@@ -4,7 +4,7 @@ Route::group(
     [
         'prefix' => 'api/lessons',
         'namespace' => 'Genuineq\Esense\Http\Controllers',
-        'middleware' => ['api', 'jwt.auth'],
+        'middleware' => ['web'],
     ],
     function () {
         Route::get('students', 'StudentsController')->name('api.lessons.students');
